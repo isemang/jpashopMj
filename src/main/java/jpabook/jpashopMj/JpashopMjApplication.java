db38885ee227adf4196ecd388d69e2c9a7e3f8bf
isemang <a55306039@gmail.com>
2021-12-31 17:56:34 +0900

    create - 인프런 JPA활용강의 프로젝트 최초 생성

---
+++ src/main/java/jpabook/jpashopMj/JpashopMjApplication.java
@@ -0,0 +1,13 @@
+package jpabook.jpashopMj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopMjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopMjApplication.class, args);
+	}
+
+}
