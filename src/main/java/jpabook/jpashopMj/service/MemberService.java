49700a5ba56430b4692b05055f631509f04a585d
isemang <a55306039@gmail.com>
2022-01-17 15:15:21 +0900

    feat - MemberService 객체 생성, 회원가입/조회 구현

---
+++ src/main/java/jpabook/jpashopMj/service/MemberService.java
@@ -0,0 +1,52 @@
+package jpabook.jpashopMj.service;
+
+import jpabook.jpashopMj.domain.Member;
+import jpabook.jpashopMj.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //@RequiredArgsConstructor를 사용하면 아래 세 줄을 쓸 필요 없음
+//    @Autowired
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    //회원 가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //문제가 있으면 예외 호출
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    @Transactional(readOnly = true)
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
