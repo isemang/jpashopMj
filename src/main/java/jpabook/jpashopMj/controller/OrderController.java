4759bbc7da4763f70ce7d749d497d893852c2875
isemang <a55306039@gmail.com>
2022-02-05 17:18:19 +0900

    feat - 주문 생성 로직 추가

---
+++ src/main/java/jpabook/jpashopMj/controller/OrderController.java
@@ -0,0 +1,44 @@
+package jpabook.jpashopMj.controller;
+
+import jpabook.jpashopMj.domain.Member;
+import jpabook.jpashopMj.domain.item.Item;
+import jpabook.jpashopMj.service.ItemService;
+import jpabook.jpashopMj.service.MemberService;
+import jpabook.jpashopMj.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
